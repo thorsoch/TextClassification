72ef82b4cac99b17e10d35b30c5004ed4ff98bbe
Zubairali Marediya <zubair_marediya@berkeley.edu>
2015-12-05 14:17:01 -0800

    updates

--- boosttester.py
+++ boosttester.py
@@ -10,16 +10,16 @@ import numpy as np
 import pickle
 import re
 
-print("testword.csv is opening")
+print("testpowerNB.csv is opening")
 
 # trainingwordpower trainingword
-with open("testword.csv", 'rU') as f:  #opens PW file
+with open("testpowerNB.csv", 'rU') as f:  #opens PW file
 	reader = csv.reader(f)
 	testmatrix = list(list(rec) for rec in csv.reader(f, delimiter=','))
 
-print("boostwordmodel is opening")
+print("boostpowerNBmodel is opening")
 
-with open("boostwordmodel", "rb") as f: #"goodSVM"
+with open("boostpowerNBmodel", "rb") as f: #"goodSVM"
 	goodmodel = pickle.load(f)
 
 print("Splitting labels and predictors")
@@ -65,7 +65,7 @@ x = [["id", "category"]] + x
 
 print("Writing out predictions")
 
-with open("testboostwordpred.csv", "wb") as f:
+with open("testboostpowerpred.csv", "wb") as f:
 	writer = csv.writer(f)
 	writer.writerows(x)
 	
